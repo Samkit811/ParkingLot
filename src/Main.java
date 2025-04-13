83e71711b45537c1e5d23f71d3f0719eb9115a25
samkits <samkitsalot0@gmail.com>
2025-04-13 10:07:12 +0530

    Add a plan how to approach problem

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,35 @@
+/**
+ * Parking lot mechanism
+ *
+ * Basic Design
+ * vehicle with 2 wheel - one floor for them
+ * vehicle with 4 wheel - one floor for them
+ * 1 entry point
+ * 1 exit point
+ *
+ * Complex Design : Any parameter can be multiple
+ */
+
+/**
+ * flow of parking
+ * first vehicle arrive - note time
+ * entry point checking that slot is available or not - give receipt
+ * go to the floor
+ * when ou want to move out
+ * exit point arrival
+ * submit receipt
+ * pay charge
+ */
+
+/**
+ * CLass list
+ * building
+ * floor
+ * vehicle >> 2 wheel and 4 wheel
+ * Entry point
+ * Exit point
+ */
+
 public class Main {
     public static void main(String[] args) {
 

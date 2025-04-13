f977e2c97e388f522ab80bb834fece9995fc1db3
samkits <samkitsalot0@gmail.com>
2025-04-13 10:43:00 +0530

    Added the exception message in the project.

--- src/EntryGate.java
+++ src/EntryGate.java
@@ -8,13 +8,18 @@ public class EntryGate {
     }
 
     public void bookSlot(Vehicle vehicle){
+        boolean isSlotBooked = false;
         for (Map.Entry<Integer, Floor> entry : this.building.floorMap.entrySet()) {
             Integer floorNumber = entry.getKey();
             Floor floor = entry.getValue();
             if(floor.bookSlot(vehicle)){
+                isSlotBooked = true;
                 System.out.println("Congratulation We have booked a slot for you on floor: " + floorNumber);
                 break;
             }
+        }
+        if(!isSlotBooked){
+            throw new OutOfParkingSpace("Sorry, We don't have enough space to park your Vehicle");
         }
     }
 }
--- src/ExitGate.java
+++ src/ExitGate.java
@@ -8,9 +8,15 @@ public class ExitGate {
     public void payParkingFee(Vehicle vehicle){
         int vehicleFloorNumber = vehicle.getFloorNumber();
         Floor floor = this.building.floorMap.get(vehicleFloorNumber);
+        if(floor == null){
+            throw new InvalidFloor("There is no floor value assign to this vehicle i.e. this vehicle is not parked in this facility");
+        }
         Slot slot = floor.slotMap.get(vehicle.vehicleNumber);
-        int parkingFee = slot.slotPrize();
-        System.out.println("You have to pay parking fee: " + parkingFee);
-        floor.emptySlot(vehicle);
+        if(floor.emptySlot(vehicle)){
+            int parkingFee = slot.slotPrize();
+            System.out.println("You have to pay parking fee: " + parkingFee);
+        } else {
+            throw new InvalidVehicleInfo("Your vehicle information is wrong.");
+        }
     }
 }

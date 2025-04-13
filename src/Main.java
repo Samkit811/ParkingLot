2eb715ece454ea73a1a327c4faa14c084feecdd6
samkits <samkitsalot0@gmail.com>
2025-04-13 10:43:29 +0530

    Added the example of how to run this project.

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,17 @@
 
 public class Main {
     public static void main(String[] args) {
+        Building building = new Building(1, 10);
+        EntryGate entryGate = new EntryGate(building);
+        ExitGate exitGate = new ExitGate(building);
 
+        Vehicle vehicle1 = new Vehicle(VehicleType.TWO_WHEELER, "GJ-04-1111");
+        Vehicle vehicle2 = new Vehicle(VehicleType.FOUR_WHEELER, "GJ-04-1234");
+
+        entryGate.bookSlot(vehicle1);
+        entryGate.bookSlot(vehicle2);
+
+        exitGate.payParkingFee(vehicle1);
+        exitGate.payParkingFee(vehicle2);
     }
 }
